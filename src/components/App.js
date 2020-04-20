7094f17fef47c7fc9f2156a90f5068e5b3fff918
Wonmi <wonmadesign@gmail.com>
2020-04-20 02:37:22 -0500

    Initial settup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PostList from './PostList';
+
+// class로 할지 functional로 할지 멈칫함. 샘은 Functional로 하셨음
+
+const App = () => {
+    return (
+        <div>
+            <PostList />
+        </div>
+    );
+}
+
+export default App;
