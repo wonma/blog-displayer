0a7f21b488c80c444850d98f7d57bb754e1f5a1a
Wonmi <wonmadesign@gmail.com>
2020-04-22 20:37:13 -0500

    Created base of PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>Post List is this!</div>
@@ -8,4 +14,4 @@ class PostList extends React.Component {
     }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts } )(PostList);
